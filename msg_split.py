4e2cf0eb6462f7540cc13a01823e4fee167e1f3c
legion <acemoving13@gmail.com>
2024-12-02 15:00:32 +0500

    wise use of generators

--- msg_split.py
+++ msg_split.py
@@ -1,17 +1,18 @@
 from typing import Generator
 from bs4 import BeautifulSoup, Tag, NavigableString
+from typing import Generator
+from bs4 import BeautifulSoup, Tag, NavigableString
 
 MAX_LEN = 4096
 BLOCK_TAGS = {'p', 'b', 'strong', 'i', 'ul', 'ol', 'div', 'span'}
 
 def split_message(source: str, max_len=MAX_LEN) -> Generator[str, None, None]:
     soup = BeautifulSoup(source, 'html.parser')
-    fragments = []
     current_fragment = ''
     open_tags = []
 
     def process_node(node):
-        nonlocal current_fragment, open_tags, fragments
+        nonlocal current_fragment, open_tags
 
         if isinstance(node, NavigableString):
             text = str(node)
@@ -21,10 +22,12 @@ def split_message(source: str, max_len=MAX_LEN) -> Generator[str, None, None]:
                 closing_tags = ''.join(f'</{tag}>' for tag in reversed(open_tags))
                 space_left = max_len - len(current_fragment) - len(closing_tags)
                 if space_left <= 0:
+                    yield current_fragment + closing_tags
                     #места нет, значит закрываем фрагмент
-                    fragments.append(current_fragment + closing_tags)
+                    # fragments.append(current_fragment + closing_tags)
                     #начинаем новый фрагмент
                     current_fragment = ''.join(f'<{tag}>' for tag in open_tags)
+                    
                     continue 
                 chunk = text[idx:idx+space_left]
                 current_fragment += chunk
@@ -39,8 +42,8 @@ def split_message(source: str, max_len=MAX_LEN) -> Generator[str, None, None]:
                 potential_length = len(current_fragment) + len(start_tag) + len(end_tag) + len(closing_tags)
                 if potential_length > max_len:
                     #места нет, значит закрываем фрагмент
-                    fragments.append(current_fragment + closing_tags)
-                    
+                    # fragments.append(current_fragment + closing_tags)
+                    yield (current_fragment + closing_tags)
                     #начинаем новый фрагмент
                     current_fragment = ''.join(f'<{tag}>' for tag in open_tags)
                     
@@ -49,12 +52,12 @@ def split_message(source: str, max_len=MAX_LEN) -> Generator[str, None, None]:
                     if potential_length > max_len and not node.contents:
                         #если даже пустой тег не помещается делаем исключение
                         raise ValueError("Block Element too long", current_fragment+closing_tags, len(current_fragment+closing_tags))
-
+                    
                 current_fragment += start_tag
                 open_tags.append(node.name)
         
                 for child in node.contents:
-                    process_node(child)
+                    yield from process_node(child)
                     
                 
                 closing_tags = ''.join(f'</{tag}>' for tag in reversed(open_tags))
@@ -62,10 +65,11 @@ def split_message(source: str, max_len=MAX_LEN) -> Generator[str, None, None]:
                     current_fragment += end_tag
                     open_tags.pop()
                     closing_tags = ''.join(f'</{tag}>' for tag in reversed(open_tags))
-                   
-                    fragments.append(current_fragment + closing_tags)
+                    yield (current_fragment + closing_tags)
+                    # fragments.append(current_fragment + closing_tags)
                     
                     current_fragment = ''.join(f'<{tag}>' for tag in open_tags)
+                    
                 else:
                     current_fragment += end_tag
                     open_tags.pop()
@@ -77,24 +81,27 @@ def split_message(source: str, max_len=MAX_LEN) -> Generator[str, None, None]:
                 if len(full_tag_str) > max_len:
                     raise ValueError("Non block tag text too long", node.name, full_tag_str, len(full_tag_str))
                 if len(current_fragment) + len(full_tag_str) + len(closing_tags) > max_len:
-                    fragments.append(current_fragment + closing_tags)
+                    yield (current_fragment + closing_tags)
+                    # fragments.append(current_fragment + closing_tags)
                     current_fragment = ''.join(f'<{tag}>' for tag in open_tags)
                     closing_tags = ''.join(f'</{tag}>' for tag in reversed(open_tags))
                     if len(full_tag_str) + len(current_fragment) + len(closing_tags) > max_len:
                         raise ValueError("Nonblock tag too long" ,node.name , current_fragment+full_tag_str+closing_tags, len(current_fragment+ full_tag_str+closing_tags))
-
+                    
                 current_fragment += full_tag_str
 
     for child in soup.contents:
-        process_node(child)
+        yield from process_node(child)
 
     if current_fragment:
         #закрываем все открытые теги 
         closing_tags = ''.join(f'</{tag}>' for tag in reversed(open_tags))
-        fragments.append(current_fragment + closing_tags)
-
-    for fragment in fragments:
+        fragment = (current_fragment + closing_tags)
         yield fragment
+
+    
+        
+
 
         
         
